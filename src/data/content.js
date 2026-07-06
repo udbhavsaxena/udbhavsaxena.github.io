512ac6b700046487e556a4a44837b27017add007
Udbhav Saxena <udbhav.saxena@gmail.com>
2026-07-06 00:06:25 -0400

    Switch live resume to version 4

--- src/data/content.js
+++ src/data/content.js
@@ -9,7 +9,7 @@ export const meta = {
   email: "udbhav.saxena@gmail.com",
   github: "https://github.com/udbhavsaxena",
   linkedin: "https://www.linkedin.com/in/udbhav-saxena/",
-  resume: "/Udbhav_Saxena_Resume-3.pdf",
+  resume: "/Udbhav_Saxena_Resume-4.pdf",
   photo: "/e.png",
 };
 
